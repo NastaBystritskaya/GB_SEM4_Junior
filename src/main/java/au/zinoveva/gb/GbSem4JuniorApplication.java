2f041b2142315d9fdb2e98bd5e8d907f615d21f9
NastaBystritskaya <nasta.bystritskaya@gmail.com>
2024-01-30 18:49:16 +0300

    Начало

---
+++ src/main/java/au/zinoveva/gb/GbSem4JuniorApplication.java
@@ -0,0 +1,13 @@
+package au.zinoveva.gb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GbSem4JuniorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GbSem4JuniorApplication.class, args);
+    }
+
+}
